690d09fa28a0d4baf876e6ab8627bf1e2ac7b39d
Lasse Rautiainen <lasse@bonecode.com>
2015-05-13 18:37:57 +0300

    Added progress bar and string grid.

--- BCControls/Source/BCControls.Register.pas
+++ BCControls/Source/BCControls.Register.pas
@@ -7,7 +7,7 @@ uses
   BCControls.RadioButton, BCControls.ToolBar, BCControls.Panel, BCControls.StatusBar, BCControls.Button,
   BCControls.SpeedButton, BCControls.Splitter, BCControls.Labels, BCControls.PageControl, BCControls.FileControl,
   BCControls.Edit, BCControls.ImageList, BCControls.ButtonedEdit, BCControls.ProgressBar, BCControls.ColorComboBox,
-  BCControls.GroupBox, BCControls.ScrollBox, BCControls.DateEdit;
+  BCControls.GroupBox, BCControls.ScrollBox, BCControls.DateEdit, BCControls.ProgressPanel, BCControls.StringGrid;
 
 procedure Register;
 
@@ -18,7 +18,7 @@ begin
   RegisterComponents('BCControls', [TBCGroupLabel, TBCComboBox, TBCFontComboBox, TBCCheckBox, TBCRadioButton,
     TBCToolBar, TBCPanel, TBCStatusBar, TBCButton, TBCSpeedButton, TBCSplitter, TBCLabel, TBCLabelFX, TBCPageControl,
     TBCDriveComboBox, TBCFileTypeComboBox, TBCFileTreeView, TBCEdit, TBCImageList, TBCButtonedEdit, TBCProgressBar,
-    TBCColorComboBox, TBCGroupBox, TBCScrollBox, TBCDateEdit]);
+    TBCColorComboBox, TBCGroupBox, TBCScrollBox, TBCDateEdit, TBCProgressPanel, TBCStringGrid]);
 end;
 
 end.
